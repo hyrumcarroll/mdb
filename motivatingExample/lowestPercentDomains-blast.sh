07f5524a0ab42dee8d82e8acb6aec03301b6f53a
Hyrum Carroll <hyrumcarroll@gmail.com>
2018-11-23 14:10:58 -0600

    Added -norandomirrevelant

--- motivatingExample/lowestPercentDomains-blast.sh
+++ motivatingExample/lowestPercentDomains-blast.sh
@@ -1,12 +1,21 @@
 for i in `cat lowestPercentDomains-queries.txt`; do
     echo $i
-    grep -A 1 ">$i" singleDomains.fa > $i.fa
-    cmd="blastp -db singleDomainDB -query $i.fa -evalue 1000 -num_descriptions 10000 -num_alignments 10000 > $i.blastOut"
-    echo $cmd
-    eval $cmd
+
+    fastaFilename="$i.fa"
+    if [ ! -s "$fastaFilename" ]; then
+	grep -A 1 ">$i" singleDomains.fa > $i.fa
+    fi
+    
+    blastOutFilename="$i.blastOut"
+    if [ ! -s "$blastOutFilename" ]; then 
+	cmd="blastp -db singleDomainDB -query $i.fa -evalue 1000 -num_descriptions 10000 -num_alignments 10000 > $blastOutFilename"
+	echo $cmd
+	eval $cmd
+    fi
+    
     family=`grep "^$i" relevancy.tab | cut -f 2`
     echo "family: $family"
-    cmd="../benchmarkingScripts/classifyRelevance.pl  --taxon=$i  --family=$family  --blastp=$i.blastOut  --rel=relevancy.tab  --spougeExt --spouge=$i.spouge1000"
+    cmd="../benchmarkingScripts/classifyRelevance.pl  --taxon=$i  --family=$family  --blastp=$blastOutFilename  --rel=relevancy.tab  --spougeExt --spouge=$i.spouge1000  --norandomsAsIrrelevants"
     echo $cmd
     eval $cmd
 done
